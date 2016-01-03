d77a3564e3f1d7707f9c37f0e9e6f98590507a67
mhd-asif <mhd.asif.1988@gmail.com>
2016-01-03 16:41:35 +0600

    Lab 16 – Adding Client side validation

---
+++ TestApplication/Scripts/Validations.js
@@ -0,0 +1,66 @@
+﻿function IsFirstNameEmpty() {
+    if (document.getElementById("TxtFName") == "") {
+        return "First Name should not be empty";
+    } else {
+        return "";
+    }
+}
+
+function IsFirstNameInValid() {
+    if (document.getElementById("TxtFName").value.indexOf("@") != -1) {
+        return "First Name should not contain @";
+    }
+    else { return ""; }
+}
+
+function IsLastNameInValid() {
+    if (document.getElementById("TxtLName").value.length >= 5) {
+        return "Last Name should not contain more than 5 character";
+    }
+    else { return ""; }
+}
+
+function IsSalaryEmpty() {
+    if (document.getElementById("TxtSalary").value == "") {
+        return "Salary should not be empty";
+    }
+    else { return ""; }
+}
+
+function IsSalaryInValid() {
+    if (isNaN(document.getElementById("TxtSalary").value)) {
+        return "Enter a valid salary";
+    }
+    else { return ""; }
+}
+
+
+function IsValid() {
+
+    var firstNameEmptyMessage = IsFirstNameEmpty();
+    var firstNameInValidMessage = IsFirstNameInValid();
+    var lastNameInValidMessage = IsLastNameInValid();
+    var salaryEmptyMessage = IsSalaryEmpty();
+    var salaryInvalidMessage = IsSalaryInValid();
+
+    var finalErrorMessage = "Errors:";
+
+    if (firstNameEmptyMessage != "")
+        finalErrorMessage += "\n" + firstNameEmptyMessage;
+    if (firstNameInValidMessage != "")
+        finalErrorMessage += "\n" + firstNameInValidMessage;
+    if (lastNameInValidMessage != "")
+        finalErrorMessage += "\n" + lastNameInValidMessage;
+    if (salaryEmptyMessage != "")
+        finalErrorMessage += "\n" + salaryEmptyMessage;
+    if (salaryInvalidMessage != "")
+        finalErrorMessage += "\n" + salaryInvalidMessage;
+
+    if (finalErrorMessage != "Errors:") {
+        alert(finalErrorMessage);
+        return false;
+    }
+    else {
+        return true;
+    }
+}
